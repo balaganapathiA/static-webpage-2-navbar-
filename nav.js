a782b3cc244ca60b853668eaf7af10716cd15698
balaganapathiA <115018255+balaganapathiA@users.noreply.github.com>
2023-12-28 11:34:30 +0530

    Update nav.js

--- nav.js
+++ nav.js
@@ -4,3 +4,24 @@ const ul=document.querySelector('ul')
 menu.addEventListener('click',()=>{
     ul.classList.toggle('show')
 })
+
+const container=document.querySelector('.container')
+const right=document.querySelector('.right')
+const left=document.querySelector('.left')
+const colors=['red','yellow','green','blue']
+
+index=0
+right.addEventListener('click',()=>{
+    index++;
+    if(index>colors.length-1){
+        index=0
+    }
+    container.style.background=colors[index]
+})
+left.addEventListener('click',()=>{
+    index--;
+    if(index<0){
+        index=3
+    }
+    container.style.background=colors[index]
+})
